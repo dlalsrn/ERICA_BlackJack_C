84796ce8fadda3bd0233e393ec827ab4209c7ff5
dlalsrn <qazzaq1541@gmail.com>
2019-11-11 16:45:18 +0900

    first commit : Add all function to use

--- BlackJack.c
+++ BlackJack.c
@@ -6,6 +6,36 @@ typedef struct deck
 	char value;
 }Deck;
 
+void ary_swap(Deck* a, Deck* b)
+{
+
+}
+
+void ary_shuffle(Deck a[], int n)
+{
+
+}
+
+Deck hit(Deck a[], int n)
+{
+
+}
+
+int more(void)
+{
+	
+}
+
+void show_cards(Deck a[])
+{
+
+}
+
+int score(Deck a[])
+{
+
+}
+
 int main(void)
 {
 	return 0;
